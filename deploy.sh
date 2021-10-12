f4cc02e9700fa378e42e9521f9ec710faf085924
amendx <amacve@gmail.com>
2021-10-12 09:42:53 -0300

    fix(deploy)

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env sh
 
-# abort on errors
 set -e
 
 # build
 npm run docs:build
-
-# navigate into the build output directory
 cd docs/.vuepress/dist
 
 
@@ -15,7 +12,6 @@ git add -A
 git commit -m 'docs(deploy)'
 
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:amendx/vue-dndrop.git master:gh-pages
 
 cd -
